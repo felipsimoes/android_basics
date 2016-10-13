6d1431a79c464763a5a96766ed25058dadac44ed
Felipe Lima Simões <felipe_lima_simoes@hotmail.com>
2016-10-13 00:39:38 -0300

    updating JustJava logic increment / decrement

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,13 +1,73 @@
-package com.example.android.justjava;
+package com.example.android.justjava; /**
+ * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ * <p>
+ * package com.example.android.justjava;
+ */
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
+import com.example.android.justjava.R;
+
+import java.text.NumberFormat;
+
+import static android.R.attr.required;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+    int price = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(quantity);
+        displayPrice(quantity * price);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        quantity++;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        quantity--;
+        display(quantity);
     }
 }
