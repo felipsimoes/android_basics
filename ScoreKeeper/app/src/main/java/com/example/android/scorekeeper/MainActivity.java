270afdc21a8836d0887f65fd4aa628f966f8e539
Felipe Lima Simões <felipe_lima_simoes@hotmail.com>
2016-10-20 18:47:47 -0200

    ScoreKeeper reviewed and improved

--- ScoreKeeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ ScoreKeeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -7,65 +7,71 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
+    private int scoreTeamA = 0;
+    private int scoreTeamB = 0;
+
+    private TextView scoreTeamAView;
+    private TextView scoreTeamBView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scoreTeamAView = (TextView) findViewById(R.id.team_a_score);
+        scoreTeamBView = (TextView) findViewById(R.id.team_b_score);
+
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
     }
 
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+        scoreTeamAView.setText(String.valueOf(score));
     }
 
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
+        scoreTeamBView.setText(String.valueOf(score));
     }
 
-    public void addSixPointsForTeamA(View view){
-        scoreTeamA+=6;
-        displayForTeamA(scoreTeamA);
+    public static void updateCounter(TextView textView, int score){
+        textView.setText(String.valueOf(score));
     }
 
-    public void addThreePointsForTeamA(View view){
-        scoreTeamA+=3;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void addTwoPointsForTeamA(View view){
-        scoreTeamA+=2;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void addOnePointForTeamA(View view){
-        scoreTeamA++;
-        displayForTeamA(scoreTeamA);
-    }
-
-    public void addSixPointsForTeamB(View view){
-        scoreTeamB+=6;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void addThreePointsForTeamB(View view){
-        scoreTeamB+=3;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void addTwoPointsForTeamB(View view){
-        scoreTeamB+=2;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void addOnePointForTeamB(View view){
-        scoreTeamB++;
-        displayForTeamB(scoreTeamB);
+    public void onSubmit(View view){
+        switch (view.getId()){
+            case R.id.button_A6:
+                scoreTeamA+=6;
+                updateCounter(scoreTeamAView,scoreTeamA);
+                break;
+            case R.id.button_A3:
+                scoreTeamA+=3;
+                updateCounter(scoreTeamAView,scoreTeamA);
+                break;
+            case R.id.button_A2:
+                scoreTeamA+=2;
+                updateCounter(scoreTeamAView,scoreTeamA);
+                break;
+            case R.id.button_A1:
+                scoreTeamA++;
+                updateCounter(scoreTeamAView,scoreTeamA);
+                break;
+            case R.id.button_B6:
+                scoreTeamB+=6;
+                updateCounter(scoreTeamBView,scoreTeamB);
+                break;
+            case R.id.button_B3:
+                scoreTeamB+=3;
+                updateCounter(scoreTeamBView,scoreTeamB);
+                break;
+            case R.id.button_B2:
+                scoreTeamB+=2;
+                updateCounter(scoreTeamBView,scoreTeamB);
+                break;
+            case R.id.button_B1:
+                scoreTeamB++;
+                updateCounter(scoreTeamBView,scoreTeamB);
+                break;
+        }
     }
 
     public void resetScores(View view){
